b30ef8a69736dd12084988826bea8e303c369d4f
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-05-28 21:40:33 +0330

    Install React Router + Set up our Routes

--- Floating-Island-Portfolio/src/App.jsx
+++ Floating-Island-Portfolio/src/App.jsx
@@ -1,7 +1,17 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
 export default function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-400">Hello world!</h1>
-    </div>
+    <main className="bg-slate-300/20">
+      <Router>
+        Navbar
+        <Routes>
+          <Route path="/" element={'Home'} />
+          <Route path="/about" element={'About'} />
+          <Route path="/projects" element={'Projects'} />
+          <Route path="/contact" element={'Contact'} />
+        </Routes>
+      </Router>
+    </main>
   );
 }
