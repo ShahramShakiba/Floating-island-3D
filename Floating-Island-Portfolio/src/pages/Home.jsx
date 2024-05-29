bb029919e91969db7050b05ec5cbfc7d36fe81f6
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-05-29 15:53:22 +0330

    Add the Island to the Scene

--- Floating-Island-Portfolio/src/pages/Home.jsx
+++ Floating-Island-Portfolio/src/pages/Home.jsx
@@ -8,6 +8,23 @@ import Loader from '../components/Loader';
 </div> */
 }
 export default function Home() {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
       <Canvas
@@ -21,7 +38,11 @@ export default function Home() {
           <spotLight />
           <hemisphereLight />
 
-          <Island />
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
         </Suspense>
       </Canvas>
     </section>
