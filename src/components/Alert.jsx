549959456a48c083b219b744687aa086ab8cd446
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-06-01 20:13:14 +0330

    Update Alert

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -3,7 +3,7 @@ export default function Alert({ type, text }) {
     'text-white bg-gradient-to-r from-[#4fd7fd83] to-[#0073fff4] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-xl';
 
   return (
-    <div className="absolute top-16 left-0 right-0 md:right-36 flex items-center justify-center md:justify-end  ">
+    <div className="absolute top-16 left-0 right-0 flex items-center justify-center">
       <div
         className={`${
           type === 'danger' ? 'bg-rose-700' : success
@@ -13,7 +13,7 @@ export default function Alert({ type, text }) {
         <p
           className={`${
             type === 'danger' ? 'bg-rose-400' : 'bg-blue-500'
-          } px-2 py-1 mr-3 font-medium flex rounded-s-2xl uppercase text-xs mt-0.5`}
+          } text-blue-100 px-2 py-1 mr-3 font-medium flex rounded-s-2xl uppercase text-xs mt-0.5`}
         >
           {type === 'danger' ? 'Failed' : 'Success'}
         </p>
